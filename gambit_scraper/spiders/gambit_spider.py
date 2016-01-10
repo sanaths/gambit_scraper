078364708f3c2a702cb9d7ccb4e4a8841ccb226d
Sanath Sambamoorthi <sanath001@gmail.com>
2016-01-10 17:28:26 -0600

    updating the spider to go through multiple pages

--- gambit_scraper/spiders/gambit_spider.py
+++ gambit_scraper/spiders/gambit_spider.py
@@ -1,51 +1,56 @@
 from meta import *
 import scrapy
 from emailer import *
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
 
 
-class GambitSpider(scrapy.Spider):
+class GambitSpider(CrawlSpider):
+
     name = "event_spider"
     allowed_domains = ["bestofneworleans.com"]
     start_urls = [
         "http://www.bestofneworleans.com/gambit/EventSearch?eventSection=1222876&narrowByDate=Next%207%20Days&neighborhoodGroup=1287922",
     ]
 
-    def parse(self, response):
+    rules = (
+            Rule(
+                    SgmlLinkExtractor(allow=(), restrict_xpaths=('//a[@class="next"]',)),
+                    callback="parse_items", follow=True
+                ),
+            )
 
-        basic_list = []
-        for event_listing in response.css('div.EventListing'):
+    def parse_items(self, response):
+            basic_list = []
 
-            basic_item = GambitScraperItem(
-                    link=event_listing.xpath('.//h3/a/@href').extract(),
-                    time=event_listing.xpath('.//div[@class="listing"]/text()').extract(),
-                    location=[
-                                event_listing.xpath('.//div[@class="listingLocation"]/a/text()').extract(),
-                                event_listing.xpath('.//div[@class="listingLocation"]/text()').extract()
-                              ],
-                    description=event_listing.xpath('.//p/text()').extract(),
-                    title=event_listing.xpath('.//h3/a/text()').extract())
+            for event_listing in response.css('div.EventListing'):
 
-            cleaned_item = self.clean_up_item_lists(basic_item)
-            print(cleaned_item)
+                basic_item = GambitScraperItem(
+                        link=event_listing.xpath('.//h3/a/@href').extract(),
+                        time=event_listing.xpath('.//div[@class="listing"]/text()').extract(),
+                        location=[
+                                    event_listing.xpath('.//div[@class="listingLocation"]/a/text()').extract(),
+                                    event_listing.xpath('.//div[@class="listingLocation"]/text()').extract()
+                                  ],
+                        description=event_listing.xpath('.//p/text()').extract(),
+                        title=event_listing.xpath('.//h3/a/text()').extract())
 
-            final_item = self.remove_empty_list_values_from_item(cleaned_item)
+                cleaned_item = self.clean_up_item_lists(basic_item)
+                final_item = self.remove_empty_list_values_from_item(cleaned_item)
 
-            basic_list.append(final_item)
+                basic_list.append(final_item)
 
-        print(basic_list)
+            print(basic_list)
 
-        emailer = Emailer()
-        message = emailer.create_message(sender="sanath001@gmail.com",
-                                         to="vfanola@gmail.com",
-                                         subject="This Week in NOLA:",
-                                         message_text=basic_list)
+            # emailer = Emailer()
+            # message = emailer.create_message(sender="sanath001@gmail.com",
+            #                                  to="vfanola@gmail.com",
+            #                                  subject="This Week in NOLA:",
+            #                                  message_text=basic_list)
+            #
+            # draft = emailer.create_draft("me", message)
 
-        draft = emailer.create_draft("me", message)
-
-
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+            return basic_list
 
     @classmethod
     def clean_up_item_lists(cls, item):
@@ -60,7 +65,7 @@ class GambitSpider(scrapy.Spider):
     @classmethod
     def remove_empty_list_values_from_item(cls, item):
 
-        link =' '.join(item['link'])
+        link = ' '.join(item['link'])
         time = ' '.join(item['time'])
         location= ' '.join(item['location'])
         description = ' '.join(item['description'])
@@ -73,7 +78,6 @@ class GambitSpider(scrapy.Spider):
                           title=title)
 
         return item
-
 
 
 
